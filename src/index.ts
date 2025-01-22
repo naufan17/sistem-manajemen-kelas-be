da28372960b54bbf511c4ecd58692ee4dbe45ec8
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2025-01-22 09:59:09 +0700

    chore: initialize project structure

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const port: number = Number(process.env.PORT) || 8000;
+const app: Express = express();
+const host: string = process.env.HOST || "localhost";
+
+app.use(morgan("combined"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`[server] Server is running on http://${host}:${port}`);
+});
+
+export default app;
